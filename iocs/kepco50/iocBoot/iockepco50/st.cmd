cf7790b6ca5395716b093cd49006e7ea6d8b412d
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2014-07-22 14:01:42 +0000

    Update comments
    Former-commit-id: 59e612a73bdf231f821f72aacbb2fcde5d8ed352

--- iocs/kepco50/iocBoot/iockepco50/st.cmd
+++ iocs/kepco50/iocBoot/iockepco50/st.cmd
@@ -6,7 +6,7 @@
 < envPaths
 
 epicsEnvSet "IOCNAME" "$(P=$(MYPVPREFIX))KEPCO50"
-epicsEnvSet "IOCSTATS_DB" "$(DEVIOCSTATS)/db/iocAdminSoft.db"
+#epicsEnvSet "IOCSTATS_DB" "$(DEVIOCSTATS)/db/iocAdminSoft.db"
 epicsEnvSet "STREAM_PROTOCOL_PATH" "$(TOP)/../../kepcoApp/protocol"
 epicsEnvSet "TTY" "$(TTY=\\\\\\\\.\\\\COM18)"
 
@@ -16,11 +16,11 @@ cd ${TOP}
 dbLoadDatabase "dbd/kepco50.dbd"
 kepco50_registerRecordDeviceDriver pdbbase
 
-drvAsynSerialPortConfigure("L0", "$(TTY)", 0, 0, 0, 0)
-asynSetOption("L0", -1, "baud", "9600")
-asynSetOption("L0", -1, "bits", "8")
-asynSetOption("L0", -1, "parity", "none")
-asynSetOption("L0", -1, "stop", "1")
+#drvAsynSerialPortConfigure("L0", "$(TTY)", 0, 0, 0, 0)
+#asynSetOption("L0", -1, "baud", "9600")
+#asynSetOption("L0", -1, "bits", "8")
+#asynSetOption("L0", -1, "parity", "none")
+#asynSetOption("L0", -1, "stop", "1")
 
 ## Load record instances
 dbLoadRecords("$(TOP)/../../db/kepco.db","P=$(IOCNAME):, PORT=L0")
